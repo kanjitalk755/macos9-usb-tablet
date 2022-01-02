502f1daf2eef0c4e670dc81cb803a8d465c2710a
kanjitalk755 <kanjitalk755@yahoo.co.jp>
2022-01-02 17:43:27 +0900

    Add icons

--- INIT.c
+++ INIT.c
@@ -1,5 +1,38 @@
 #include <A4Stuff.h>
 
+#define	LMVCheckSum			(*(UInt16 *)0x928)
+#define	LMVCoord			(*(UInt16 *)0x92A)
+#define	LMHCoord			(*(UInt16 *)0x92C)
+#define	LMHCheckSum			(*(UInt16 *)0x92E)
+#define	CheckSum(x)			(((x) << 1 | (x) >> 15) ^ 0x1021)
+
+static void ShowInitIcon(void) {
+	UInt32 saveA5;
+	QDGlobals qd;
+	UInt32 qdGlobalsPtr;
+	CGrafPort colorPort;
+	Point pt;
+	
+	saveA5 = SetA5((UInt32)&qdGlobalsPtr);
+	InitGraf(&qd.thePort);
+	
+	if (CheckSum(LMHCoord) != LMHCheckSum) LMHCoord = 8;
+	if (CheckSum(LMVCoord) != LMVCheckSum) LMVCoord = qd.screenBits.bounds.bottom - 40;
+	
+	pt = LMHCoord + 34 <= qd.screenBits.bounds.right ? (Point){ LMVCoord, LMHCoord } : (Point){ LMVCoord - 40, 8 };
+
+	OpenCPort(&colorPort);
+	PlotIconID(&(Rect){ pt.v, pt.h, pt.v + 32, pt.h + 32 }, atNone, ttNone, -16455);
+	CloseCPort(&colorPort);
+
+	LMHCoord = pt.h + 40;
+	LMVCoord = pt.v;
+	LMHCheckSum = CheckSum(LMHCoord);
+	LMVCheckSum = CheckSum(LMVCoord);
+		
+	SetA5(saveA5);
+}
+
 void main(void) {
 	Handle hd;
 	UInt32 saveA4 = SetCurrentA4();
@@ -19,5 +52,6 @@ void main(void) {
 		}
 	}
 	SetZone(ApplicationZone());
+	ShowInitIcon();
 	SetA4(saveA4);
 }
